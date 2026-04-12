069e26faab49c33dbb6d6b743cec06f0caac6997
Victor Pena <victor.javier.pena210@gmail.com>
2026-04-12 14:43:25 -0500

    updated the length of password to 255 and added semicolon on v3 migrations

--- src/main/resources/db/migration/V3__create_users.sql
+++ src/main/resources/db/migration/V3__create_users.sql
@@ -3,9 +3,9 @@ CREATE TABLE users (
 	first_name VARCHAR(100) NOT NULL,
 	last_name VARCHAR(100) NOT NULL,
 	email VARCHAR(255) NOT NULL,
-	password VARCHAR(20) NOT NULL,
+	password VARCHAR(255) NOT NULL,
 	role VARCHAR(20) NOT NULL,
 	enabled BOOLEAN NOT NULL DEFAULT TRUE,
 	CONSTRAINT pk_users PRIMARY KEY (id),
 	CONSTRAINT uk_users_email UNIQUE (email)
-)
+);
