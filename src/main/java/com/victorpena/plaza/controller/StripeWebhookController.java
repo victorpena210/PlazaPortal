768c80d03a2e3ff8f64743b10a6e3da26247677c
Victor Pena <victor.javier.pena210@gmail.com>
2026-05-15 16:42:21 -0500

    stripe webhook implemented

---
+++ src/main/java/com/victorpena/plaza/controller/StripeWebhookController.java
@@ -0,0 +1,55 @@
+package com.victorpena.plaza.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.stripe.exception.SignatureVerificationException;
+import com.stripe.model.Event;
+import com.stripe.net.Webhook;
+
+@RestController
+@RequestMapping("/stripe")
+public class StripeWebhookController {
+
+    @Value("${stripe.webhook.secret}")
+    private String webhookSecret;
+
+    @PostMapping("/webhook")
+    public ResponseEntity<String> handleWebhook(
+            @RequestBody String payload,
+            @RequestHeader("Stripe-Signature") String sigHeader) {
+
+        Event event;
+
+        try {
+            event = Webhook.constructEvent(
+                    payload,
+                    sigHeader,
+                    webhookSecret
+            );
+
+        } catch (SignatureVerificationException e) {
+
+            return ResponseEntity
+                    .badRequest()
+                    .body("Invalid signature");
+        }
+
+        System.out.println("Webhook verified!");
+
+        if ("checkout.session.completed".equals(event.getType())) {
+
+            System.out.println("Checkout completed!");
+
+            // TODO:
+            // Mark payment as PAID here
+        }
+
+        return ResponseEntity.ok("Success");
+    }
+}
